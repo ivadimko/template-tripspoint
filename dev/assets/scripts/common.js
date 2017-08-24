df41f24641c7951f1baf890f09fb94a430062a6c
Vadim Ilchenko <vvadimko@gmail.com>
2017-08-24 19:20:14 +0300

    sections done

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -18,17 +18,39 @@ $('.slick').slick({
   });	
 
 });
-	$('.quote__text').each(function() { 
-	if ($(this).height() < 50) {
-		$(this).next().css('display', 'none');
-	} else {
-		$(this).addClass('quote__text--overflow');
-	};
-});
-$(document).on('click','.quote__link', function(){
-	$(this).prev().removeClass('quote__text--overflow');
-	$(this).css('display','none');
-});
+
+//-----------Quote-review------------
+	$('.js-close-link').css('display', 'none');
+	$('.js-hidden-text').each(function() { 
+		if ($(this).height() < 55) {
+			$(this).css('height', 55);
+			$(this).next().css('visibility', 'hidden');
+		} else {
+			$(this).addClass('quote__text--overflow');
+		};
+	});
+	$(document).on('click','.js-open-link', function(){
+		$(this).prev().removeClass('quote__text--overflow');
+		$(this).css('visibility', 'hidden');
+		$(this).next().css('display', 'block');
+	});
+	$(document).on('click','.js-close-link', function(){
+		$(this).prev().prev().addClass('quote__text--overflow');
+		$(this).css('display', 'none');
+		$(this).prev().css('visibility', 'visible');
+	});
+//--------------------------------------------
+
+//-------------Gallery-overlay----------------
+	$('.trip__content').css('display', 'none');
+	$(document).on('click','.js-trip-link', function(){
+		$(this).parent().toggleClass('image-overlay--open');
+		$(this).parent().children('.trip__content').slideToggle();
+		$(this).parent().children('.trip__price--bg').delay(200).slideToggle(600);
+	});
+		
+
+
 });
 jQuery(window).on('load', function () {
 	mobileNav.init('.header__menu');
