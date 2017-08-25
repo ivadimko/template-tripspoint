0db25769ccd074b4608d8d35b24e6d34bc8beb51
Vadim Ilchenko <vvadimko@gmail.com>
2017-08-26 01:32:54 +0300

    mobile frendly now

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -15,9 +15,66 @@ $('.slick').slick({
    dots: true,
    appendDots: $(".more__dots"),
    autoplay: true,
+   responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        vertical: false,
+  			verticalSwiping: false,
+
+      }
+    }
+      ],
   });	
+$('.review__secondary-block').slick({
+   slidesToShow: 2,
+   slidesToScroll: 1,
+   arrows: true,
+   prevArrow: null,
+   nextArrow: $(".button__next-image"),
+   dots: true,
+   appendDots: $(".more__image-dots"),
+   autoplay: true,
+   responsive: [
+    {
+    	breakpoint: 1200,
+    	settings: {
+    		slidesToShow: 1
+    	}
+    },
+    {
+    	breakpoint: 992,
+    	settings: {
+    		slidesToShow: 2
+    	}
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+      ],
+});
 
 });
+//------Currency Dropdown menu-------
+$('.currency__list').css('display', 'none');
+$(document).on('click', '.currency', function(){
+	$(this).toggleClass('currency--open');
+	$(this).children('.currency__list').slideToggle();
+});
+$(document).on('click', '.currency__item', function(){
+	$(this).children('span').prependTo($('.currency'));
+	$('.currency').children().next('span').prependTo(this);
+	$(this).children().next().remove();
+
+});
+//-----------------------------------
 
 //-----------Quote-review------------
 	$('.js-close-link').css('display', 'none');
@@ -49,7 +106,17 @@ $('.slick').slick({
 		$(this).parent().children('.trip__price--bg').delay(200).slideToggle(600);
 	});
 //--------------------------------------------		
+//----------------Footer Nav------------------
+if ($(document).width() < 768) {
+		$('.footer__nav').css('display', 'none');
+		$(document).on('click','.footer__heading', function(){
+			$(this).next('.footer__nav').slideToggle();
+		});
+	};
+//--------------------------------------------
+//-----------------Map Marker-----------------
 
+//--------------------------------------------
 
 });
 jQuery(window).on('load', function () {
@@ -64,9 +131,10 @@ var mobileNav = {
 		if (!$(this.mobileMenuClassName).children().length) {
 			$(mainMenuClassName).children().clone().prependTo(this.mobileMenuClassName);
 		}
-
 		$(document).on('click', '.burger', function () {
 			mobileNav.toggle();
+			$('.header__burger').fadeToggle(0);
+			$('.header__logo').fadeToggle(800);
 		});
 	},
 	open: function () {
